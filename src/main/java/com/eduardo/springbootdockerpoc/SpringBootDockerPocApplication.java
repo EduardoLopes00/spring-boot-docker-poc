404ef20f740f43252eece15276c5a2fb332985c7
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2023-02-01 18:58:05 -0300

    feat: create first request

--- src/main/java/com/eduardo/springbootdockerpoc/SpringBootDockerPocApplication.java
+++ src/main/java/com/eduardo/springbootdockerpoc/SpringBootDockerPocApplication.java
@@ -2,9 +2,17 @@ package com.eduardo.springbootdockerpoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringBootDockerPocApplication {
+
+	@GetMapping("/hello-world")
+	public String testRequest() {
+
+
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDockerPocApplication.class, args);
