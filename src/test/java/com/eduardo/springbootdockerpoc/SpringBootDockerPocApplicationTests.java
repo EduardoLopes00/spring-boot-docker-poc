a52d93a79e69fa7d944f387169c893e4b7710276
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2023-02-01 18:36:23 -0300

    feat: Initial commit

---
+++ src/test/java/com/eduardo/springbootdockerpoc/SpringBootDockerPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.springbootdockerpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
